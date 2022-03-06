555e670056656e22d767b2159f13f4b0166d3e61
Daniel Eicher <d.eicher@mailbox.org>
2022-03-06 11:40:33 +0100

    Initial commit

---
+++ src/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
+export default GlobalStyles;
