3f75b1315666db891874ee80aa4e0864d91a67d6
Daniel Eicher <d.eicher@mailbox.org>
2022-03-07 21:03:45 +0100

    Add dashboard test

--- src/pages/Dashboard.spec.js
+++ src/pages/Dashboard.spec.js
@@ -2,8 +2,35 @@ import { render, screen } from '@testing-library/react';
 import Dashboard from './Dashboard';
 
 describe('Dashboard', () => {
-  it('renders', () => {
-    render(<Dashboard />);
-    expect(screen.getByText('DASHBOARD', /Daniel Eicher/)).toBeInTheDocument();
+  it('renders the Dashboard LoadingState', () => {
+    render(<Dashboard loading={true} />);
+    expect(screen.getByText('DASHBOARD')).toBeInTheDocument(); // Heading
+    expect(screen.getByAltText(/Loading/i)).toBeInTheDocument(); // Animation
+    expect(screen.getByText(/Daniel Eicher/)).toBeInTheDocument(); // Footer
+  });
+
+  it('renders the Dashboard', () => {
+    const issues = [
+      { id: 111, title: 'Teststring1', state: 'open' },
+      { id: 112, title: 'Teststring2', state: 'open' },
+    ];
+
+    render(<Dashboard issues={issues} />);
+
+    expect(screen.getByText('DASHBOARD')).toBeInTheDocument(); // Heading
+    expect(
+      screen.getByText(/Title/, /State/, /Teststring1/, /Teststring2/)
+    ).toBeInTheDocument(); // ListHeading
+    // IssueList
+    expect(screen.getByText(/Daniel Eicher/)).toBeInTheDocument(); // Footer
+  });
+
+  it('renders the Dashboard ErrorState', () => {
+    render(<Dashboard error={true} />);
+    expect(screen.getByText('DASHBOARD')).toBeInTheDocument(); // Heading
+    expect(
+      screen.getByText(/Oops, something went wrong./i)
+    ).toBeInTheDocument(); // Error text
+    expect(screen.getByText(/Daniel Eicher/)).toBeInTheDocument(); // Footer
   });
 });
